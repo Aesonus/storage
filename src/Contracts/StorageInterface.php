eaf543ff206a2030c70628238eb8a9073863eefc
Aesonus <corylcomposinger@gmail.com>
2018-03-12 22:23:39 -0500

    Initial Commit

---
+++ src/Contracts/StorageInterface.php
@@ -0,0 +1,21 @@
+<?php
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace Aesonus\Storage\Contracts;
+
+/**
+ *
+ * @author Aesonus <corylcomposinger at gmail.com>
+ */
+interface StorageInterface extends \Countable
+{
+    public function get($offset);
+    
+    public function set($offset, $value);
+    
+    public function has($offset);
+}
---
+++ src/RuntimeStorage.php
@@ -0,0 +1,38 @@
+<?php
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace Aesonus\Storage;
+
+/**
+ * Storage for a single script
+ *
+ * @author Aesonus <corylcomposinger at gmail.com>
+ */
+class RuntimeStorage implements Contracts\StorageInterface
+{
+    protected $storage;
+    
+    public function count()
+    {
+        return count($this->storage);
+    }
+
+    public function get($offset)
+    {
+        return $this->storage[$offset];
+    }
+
+    public function has($offset)
+    {
+        return $this->storage[$offset] === NULL;
+    }
+
+    public function set($offset, $value)
+    {
+        return $this->storage[$offset] = $value;
+    }
+}
