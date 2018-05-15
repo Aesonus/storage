2d746531783e294fae917ffbe5a0c0c16bb0ce05
Aesonus <corylcomposinger@gmail.com>
2018-05-14 23:27:38 -0500

    Updated LICENSE

--- src/RuntimeStorage.php
+++ src/RuntimeStorage.php
@@ -1,8 +1,6 @@
 <?php
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * This software is licensed under the MIT License. Please see LICENSE for more details.
  */
 
 namespace Aesonus\Storage;
